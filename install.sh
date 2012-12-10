75615d835dc9c7089016efdea0d725d37f678e9f
akril <akrilfx@gmail.com>
2012-12-10 13:54:48 +0000

    install.sh modified to loop through config files and create symlink

--- install.sh
+++ install.sh
@@ -1,1 +1,11 @@
 # TODO
+read -p "WARNING - this will overwrite existing files in your \$HOME. Are you sure? (y/n) " -n 1
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  for file in bash* git* tcsh*
+  do
+    ln -fs "$PWD/$file" "$HOME/.$file"
+    echo "$file --> $HOME/.$file"
+  done
+fi
+
